e7487143089230fe0f2623ce468131006fef93dd
Carlos <carlos.oli1505@gmail.com>
2020-03-02 22:10:54 -0300

    Adição de model para usuário e endereço

---
+++ LavaCar.Domain/User/AddressModel.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LavaCar.Domain.User
+{
+    public class AddressModel
+    {
+        public long AddressModelId { get; set; }
+
+        [JsonProperty("cep")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Street { get; set; }
+
+        [JsonProperty("complemento")]
+        public string Complement { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Neighborhood { get; set; }
+
+        [JsonProperty("localidade")]
+        public string City { get; set; }
+
+        [JsonProperty("uf")]
+        public string StateInitials { get; set; }
+
+        [JsonProperty("unidade")]
+        public string Unit { get; set; }
+
+        [JsonProperty("ibge")]
+        public string IBGECode { get; set; }
+
+        [JsonProperty("gia")]
+        public string GiaCode { get; set; }
+    }
+}
