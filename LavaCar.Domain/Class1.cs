eb0515b4edb64dfd0bc77b025203ef75ce97108e
Carlos <carlos.oli1505@gmail.com>
2020-03-02 21:44:59 -0300

    Criação do projeto e adição de bootstrap, DDD e Firebase

---
+++ LavaCar.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LavaCar.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ LavaCar.Web.Mvc/wwwroot/js/FirebaseConfig.js
@@ -0,0 +1,23 @@
+﻿!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.9.3/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.9.3/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAHXHuVgFp-UH2n7koQCijDK_QlFE8J3JU",
+    authDomain: "lavacar-1a4fe.firebaseapp.com",
+    databaseURL: "https://lavacar-1a4fe.firebaseio.com",
+    projectId: "lavacar-1a4fe",
+    storageBucket: "lavacar-1a4fe.appspot.com",
+    messagingSenderId: "1030921615802",
+    appId: "1:1030921615802:web:21676f651c6c0440bd8583",
+    measurementId: "G-9VSX5BL9CC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
